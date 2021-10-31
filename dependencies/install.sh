e7ad07a26f201e1d0ad909a86d8f08b9a33cafcd
gabrielbjg <gabriel.bjg@gmail.com>
2021-10-31 12:38:02 +0200

    Add dependency installation script

---
+++ dependencies/install.sh
@@ -0,0 +1,8 @@
+# Install Catch2
+git clone https://github.com/catchorg/Catch2.git
+cd Catch2
+cmake -Bbuild -H. -DBUILD_TESTING=OFF
+sudo cmake --build build/ --target install
+
+# Install cppcheck
+sudo apt-get install cppcheck
