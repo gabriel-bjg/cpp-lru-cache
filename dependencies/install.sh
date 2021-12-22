632e6cd5e0cf9450496a5416926939c030874480
gabrielbjg <gabriel.bjg@gmail.com>
2021-12-22 15:51:42 +0200

    Clone a specific Catch2 version to avoid possible future incompatibilities

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 
 # Install Catch2
-git clone https://github.com/catchorg/Catch2.git
+git clone https://github.com/catchorg/Catch2/tree/v2.13.7
 cd Catch2
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
 sudo cmake --build build/ --target install
