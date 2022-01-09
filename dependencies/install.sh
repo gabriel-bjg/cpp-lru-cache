b583eef8cfe77908fdc229895fbb75c3fe681796
gabrielbjg <gabriel.bjg@gmail.com>
2022-01-09 18:33:33 +0200

    Update catch2 version to 3.0.0-preview4

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 
 # Install Catch2
-git clone --depth 1 --branch v2.13.7 https://github.com/catchorg/Catch2.git
+git clone --depth 1 --branch v3.0.0-preview4 https://github.com/catchorg/Catch2.git
 cd Catch2
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
 sudo cmake --build build/ --target install
